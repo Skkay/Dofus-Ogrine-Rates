a75889f0e38277d8a5db52f8a74a621f3156eb27
Skkay <skkay.thomas@gmail.com>
2021-07-31 20:14:08 +0200

    Feat: Add some logging messages

--- get-ogrine-rate.py
+++ get-ogrine-rate.py
@@ -2,40 +2,67 @@ from bs4 import BeautifulSoup
 from collections import OrderedDict
 from datetime import datetime
 from json import JSONDecoder
+import logging
 import requests
 import yaml
 
 def getLatestOgrineValues():
     url = "https://www.dofus.com/fr/achat-kamas/cours-kama-ogrines"
     headers = { 'User-Agent': 'Mozilla/5.0' }
-    content = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(content)
 
-    for script in soup.head.find_all('script'):
-        if str(script).startswith('<script type="text/javascript">\n    RATES = '):
-            str_rates = str(script)
-            begin = str_rates.find("{")
-            end = str_rates.find("}")
-            rates = JSONDecoder(object_pairs_hook=OrderedDict).decode(str_rates[begin:end+1])
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-            return {'timestamp': list(rates)[-2], 'currentRate': rates[list(rates)[-2]], 'previousRate': rates[list(rates)[-3]]}
+        content = response.text
+        soup = BeautifulSoup(content)
+        for script in soup.head.find_all('script'):
+            if str(script).startswith('<script type="text/javascript">\n    RATES = '):
+                str_rates = str(script)
+                begin = str_rates.find("{")
+                end = str_rates.find("}")
+                rates = JSONDecoder(object_pairs_hook=OrderedDict).decode(str_rates[begin:end+1])
+                data = {'timestamp': list(rates)[-2], 'currentRate': rates[list(rates)[-2]], 'previousRate': rates[list(rates)[-3]]}
+
+                logging.info(f'Successfully fetch Ogrine values: {data=}')
+                return data
+
+        logging.error('No <script> tag containing "RATES" was found')
+        raise Exception('No <script> tag containing "RATES" was found')
+    except requests.exceptions.Timeout as e:
+        logging.error(f'Timeout: {str(e)}')
+        raise
+    except requests.exceptions.HTTPError as e:
+        logging.error(f'HTTPError: {str(e)}')
+        raise
+    except requests.exceptions.RequestException as e:
+        logging.error(f'RequestException: {str(e)}')
+        raise
 
 
 def writeToCsv(csv, timestamp, rate):
     with open(csv, 'a') as f:
         f.write(str(timestamp) + ',' + str(rate) + '\n')
+        logging.info(f'Successfully write to "{csv}": {timestamp=}, {rate=}')
 
 
 def sendToDiscord(url, message):
     try:
         response = requests.post(url, data={'content': message})
         response.raise_for_status()
+        logging.info(f'Successfully sent to Discord using webhook "{url}": {message=}')
+    except requests.exceptions.Timeout as e:
+        logging.error(f'Timeout: {str(e)}')
+    except requests.exceptions.HTTPError as e:
+        logging.error(f'HTTPError: {str(e)}')
     except requests.exceptions.RequestException as e:
-        print(e)
+        logging.error(f'RequestException: {str(e)}')
 
 
 with open("config.yaml") as f:
     config = yaml.safe_load(f)
+
+logging.basicConfig(filename=config['log_path'], encoding='utf-8', level=logging.DEBUG, format='[%(asctime)s.%(msecs)03d] %(levelname)s:%(module)s:%(funcName)s => %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 data = getLatestOgrineValues()
 timestamp = data['timestamp']
